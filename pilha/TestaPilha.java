45f3d9f3ab74e5668d35553f26a044362c93e073
amignon <amignon@gmail.com>
2026-09-01 13:29:33 +0000

    Exercício Pilha

--- pilha/TestaPilha.java
+++ pilha/TestaPilha.java
@@ -11,9 +11,26 @@ public class TestaPilha {
         else {
             System.out.println("Inválida!");
         }
+        entrada.close();
     }
 
     public static boolean bemFormada(String expressao) {
-
+        ArrayStack<Character> pilha = new ArrayStack<Character>();
+        int tamanho = expressao.length();
+        for (int i = 0; i < tamanho; i++) {
+            char c = expressao.charAt(i);
+            if (c == '(') {
+                pilha.push(c);
+            }
+            else if (c == ')') {
+                if (!pilha.isEmpty()) {
+                    pilha.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return pilha.isEmpty();
     }
 }
